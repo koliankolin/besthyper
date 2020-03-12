b14f732c38aca10e1e504219623311e3ceb218ea
fwelpe <arestovfa@gmail.com>
2020-03-12 18:51:18 +0300

    save

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { Link as RouterLink } from "@reach/router";
-import ts from './translate';
-/* assets */
-import frenchFlagImg from './assets/images/flag_fr.png';
-import englishFlagImg from './assets/images/flag_en.png';
-import { useMediaQuery } from '@material-ui/core';
+import React, { useState, useEffect } from 'react'
+import { Link as RouterLink } from '@reach/router'
+import ts from './translate'
+import frenchFlagImg from './assets/images/flag_fr.png'
+import englishFlagImg from './assets/images/flag_en.png'
+import { useMediaQuery } from '@material-ui/core'
 
 import {
   MenuItem,
@@ -15,23 +14,22 @@ import {
   Button,
   Menu,
   AppBar as MuiAppBar,
-} from '@material-ui/core';
+} from '@material-ui/core'
 import {
-  Home as HomeIcon,
   Person,
 } from '@material-ui/icons'
 
-/* Navbar JSS styling */
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
   },
   menuButton: {
     marginRight: theme.spacing(2),
-    
+
   },
   title: {
     flexGrow: 1,
+    'text-align': 'center',
   },
   langSwitch: {
     width: '40px',
@@ -40,7 +38,7 @@ const useStyles = makeStyles(theme => ({
   langImg: {
     width: '30px',
   },
-}));
+}))
 
 const AppBar = ({
   auth,
@@ -48,42 +46,43 @@ const AppBar = ({
   onClickLogout,
   lang,
 }) => {
-  const classes = useStyles();
-  const mediaSmall = useMediaQuery('(max-width:600px)');
-  const [anchorElem, setAnchorElem] = useState(null);
-  const [activeMenu, setActiveMenu] = useState('');
+  const classes = useStyles()
+  const mediaSmall = useMediaQuery('(max-width:600px)')
+  const [anchorElem, setAnchorElem] = useState(null)
+  const [activeMenu, setActiveMenu] = useState('')
 
   /*  side effect for active menu  */
   useEffect(() => {
     // close and reset menu infos only if it is the account menu.
     // do not apply this behaviour for language menu.
     if (!!mediaSmall === false && activeMenu === 'accountMenu') {
-      setActiveMenu('');
-      setAnchorElem(null);
+      setActiveMenu('')
+      setAnchorElem(null)
     }
   }, [mediaSmall, activeMenu])
 
-  const handleClose = () => setAnchorElem(null);
+  const handleClose = () => setAnchorElem(null)
 
   const langMenuItems = [
     { name: 'eng', selected: 'eng' === lang, img: englishFlagImg },
     { name: 'fr', selected: 'fr' === lang, img: frenchFlagImg },
-  ];
+  ]
 
   const handleLangMenu = item => {
-    onChangeLang(item);
-    handleClose();
-  };
+    onChangeLang(item)
+    handleClose()
+  }
 
   const handlemenu = (e, menu) => {
-    setAnchorElem(e.currentTarget);
-    setActiveMenu(menu);
-  };
+    setAnchorElem(e.currentTarget)
+    setActiveMenu(menu)
+  }
 
   const langSection = (
-    <>
+    <div>
       <IconButton onClick={e => handlemenu(e, 'langMenu')}>
-        <img className={classes.langImg} src={lang === 'eng' ? englishFlagImg : frenchFlagImg} alt={lang.name} />
+        <img className={classes.langImg} src={lang === 'eng' ? englishFlagImg : frenchFlagImg}
+             alt={lang.name}/>
       </IconButton>
       <Menu
         id="lang-menu"
@@ -92,31 +91,31 @@ const AppBar = ({
         open={!!anchorElem && activeMenu === 'langMenu'}
         onClose={handleClose}
       >
-        { langMenuItems.map(lang => (
+        {langMenuItems.map(lang => (
           <MenuItem
             key={lang.name}
             onClick={() => handleLangMenu(lang.name)}
             selected={lang.selected}
           >
-          <img className={classes.langImg} src={lang.img} alt={lang.name} />
-        </MenuItem>
+            <img className={classes.langImg} src={lang.img} alt={lang.name}/>
+          </MenuItem>
         ))}
       </Menu>
-    </>
-  );
+    </div>
+  )
 
   const authSection = mediaSmall ? (
     <>
       <IconButton onClick={e => handlemenu(e, 'accountMenu')}>
-        <Person />
+        <Person/>
       </IconButton>
       <Menu
-      id="lang-menu"
-      anchorEl={anchorElem}
-      keepMounted
-      open={!!anchorElem && activeMenu === 'accountMenu'}
-      onClose={handleClose}
-      onClick={handleClose}
+        id="lang-menu"
+        anchorEl={anchorElem}
+        keepMounted
+        open={!!anchorElem && activeMenu === 'accountMenu'}
+        onClose={handleClose}
+        onClick={handleClose}
       >
         <MenuItem>
           <RouterLink to="/profil">
@@ -132,22 +131,22 @@ const AppBar = ({
     <>
       <RouterLink to="/profil">
         <Button>
-          {ts.navbar.profil[lang]} 
+          {ts.navbar.profil[lang]}
         </Button>
       </RouterLink>
-        <Button
-          onClick={onClickLogout}
-          color="inherit"
-        >
-          {ts.navbar.logout[lang]}
-        </Button>
+      <Button
+        onClick={onClickLogout}
+        color="inherit"
+      >
+        {ts.navbar.logout[lang]}
+      </Button>
     </>
-  );
+  )
 
   return (
     <header className={classes.root}>
       <MuiAppBar position="static">
-        <Toolbar>
+        <Toolbar style={{ height: '90px' }}>
           <RouterLink to="/">
             <IconButton
               edge="start"
@@ -155,18 +154,17 @@ const AppBar = ({
               color="inherit"
               aria-label="menu"
             >
-              <HomeIcon />
+              <Typography variant="h6" className={classes.title}>
+                {mediaSmall ? '#HT' : 'Hypertube'}
+              </Typography>
             </IconButton>
           </RouterLink>
-          <Typography variant="h6" className={classes.title}>
-            { mediaSmall ? '#HPTB' : 'HYPERTUBE'}
-          </Typography>
-          { langSection }
-          { auth && authSection}
+          {langSection}
+          {auth && authSection}
         </Toolbar>
       </MuiAppBar>
     </header>
-  );
+  )
 }
 
-export default AppBar;
+export default AppBar
